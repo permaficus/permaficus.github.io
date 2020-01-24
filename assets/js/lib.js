cae38218c457959d703049ca31e40ef2928b3e01
permaficus <abukhalif2019@gmail.com>
2020-01-24 16:35:04 +0700

    fix some typo syntax

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -2,7 +2,7 @@ onload = () => {
 
     timeformat = (t) => {return (t < 10) ? '0' + t:t;}
     showTime = () => {
-        let now = new Date; el = document.getElementById("showtime").innerHTML = timeformat(now.getHours()) + ':'
+        let now = new Date(), el = document.getElementById("showtime").innerHTML = timeformat(now.getHours()) + ':'
                 + timeformat(now.getMinutes()) + ':' + timeformat(now.getSeconds());
         
         setTimeout(() => {
